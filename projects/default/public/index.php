3b7e60b3e367012763d6ced504420ede8ce7ad17
Alex Shcneider <alex.shcneider@gmail.com>
2018-10-03 15:56:40 +0300

    readme update

--- projects/default/public/index.php
+++ projects/default/public/index.php
@@ -2,12 +2,10 @@
 
 ini_set('html_errors', 1);
 
-
 // mysql
 $mysql = new \Mysqli('mysql', 'root', 'root');
 $result = $mysql->query("SELECT NOW()")->fetch_assoc();
-var_dump($result);
-
+var_dump($result['NOW()']);
 
 // memcached
 $memcached = new \Memcached();
@@ -15,17 +13,9 @@ $memcached->addServer('memcached', 11211);
 $memcached->set('test', time());
 var_dump($memcached->get('test'));
 
-
 // redis
 $redis = new \Redis();
 $redis->connect('redis', 6379);
 $redis->set('test', time());
 var_dump($redis->get('test'));
 
-
-// curl
-$ch = curl_init('https://www.google.com');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HEADER, true);
-$result = curl_exec($ch);
-var_dump($result); exit;
